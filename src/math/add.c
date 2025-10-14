8660dc62e52b4b6acefc406a479350b0d3ff26c6
Lilibaghoyan <lilibagoyan@gmail.com>
2025-10-15 02:33:31 +0400

    a

--- src/math/add.c
+++ src/math/add.c
@@ -1,9 +1,18 @@
 #include "dsalib/math/add.h"
 
 int unsafe_add(int a, int b) {
-    return -1;
+    return a + b;
 }
 
 int safe_add(int a, int b) {
-    return -1;
+    int sum = a + b;
+    if (a > 0 && b > 0 && sum < 0) {
+        printf("Error: integer overflow\n");
+        return 0;
+    }
+    if (a < 0 && b < 0 && sum > 0) {
+        printf("Error: integer underflow\n");
+        return 0;
+    }
+    return sum;
 }

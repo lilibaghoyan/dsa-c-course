7967cc7782f46ef67c790f339b3368e959aa833d
Lilibaghoyan <lilibagoyan@gmail.com>
2025-10-22 02:12:35 +0400

    1c

--- src/search/linear_search.c
+++ src/search/linear_search.c
@@ -3,5 +3,16 @@
 #include <stddef.h>
 
 int dsalib_linear_search(const int* arr, size_t size, int target) {
+    for (int i = 0; i < size; i++)
+    {
+        if(arr[i] == target)
+        {
+            return i;
+        }
+    }
+    if (size == 0 || !arr)
+    {
+        return -1;
+    }
     return -1;
 }

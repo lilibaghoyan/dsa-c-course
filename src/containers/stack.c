0d94fef808d44a9000df4e82e0b2a833ea53a509
Lilibaghoyan <lilibagoyan@gmail.com>
2025-11-12 11:46:09 +0400

    11.11

--- src/containers/stack.c
+++ src/containers/stack.c
@@ -16,11 +16,6 @@ dsalib_stack_t* dsalib_stack_create(size_t initial_capacity) {
     }
     
     stack->data = malloc(initial_capacity*sizeof(int));
-    if (!stack->data){
-        free(stack);
-        return NULL;
-    }
-
     stack->size = 0;
     stack->capacity = initial_capacity;
     return stack;
@@ -28,14 +23,13 @@ dsalib_stack_t* dsalib_stack_create(size_t initial_capacity) {
 
 
 void dsalib_stack_destroy(dsalib_stack_t* stack) {
-    if(!stack) return;
+    if(!stack || stack->size == 0 || !stack->data) return;
     free(stack->data);
     free(stack);
-
 }
 
 bool dsalib_stack_push(dsalib_stack_t* stack, int value) {
-    if (!stack) {
+    if(!stack || !stack->data){
         return false;
     }
     if(stack->size == stack->capacity){
@@ -53,23 +47,47 @@ bool dsalib_stack_push(dsalib_stack_t* stack, int value) {
 }
 
 bool dsalib_stack_pop(dsalib_stack_t* stack, int* value) {
-    return false;
+    if(!stack || stack->size == 0 || !stack->data){
+        return false;
+    }
+    *value = stack->data[stack->size - 1];
+    stack->size--;
+
+    return true;
 }
 
 bool dsalib_stack_peek(const dsalib_stack_t* stack, int* value) {
-    return false;
+    if(!stack || stack->size == 0 || !stack->data){
+        return false;
+    }
+    *value = stack->data[stack->size - 1];
+    return true;
 }
 
 bool dsalib_stack_is_empty(const dsalib_stack_t* stack) {
+    if(!stack || stack->size == 0 || !stack->data){
+        return true;
+    }
     return false;
 }
 
 size_t dsalib_stack_size(const dsalib_stack_t* stack) {
-    return 0;
+    if(!stack){
+        return 0;
+    }
+    return stack->size;
 }
 
 size_t dsalib_stack_capacity(const dsalib_stack_t* stack) {
-    return 0;
+    if(!stack){
+        return 0;
+    }
+    return stack->capacity;
 }
 
-void dsalib_stack_clear(dsalib_stack_t* stack) {}
+void dsalib_stack_clear(dsalib_stack_t* stack) {
+    if(!stack || stack->size == 0 || !stack->data){
+        return;
+    }
+    stack->size = 0;
+}
